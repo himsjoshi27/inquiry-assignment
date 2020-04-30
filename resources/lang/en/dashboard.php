3721277d666ae697609a9a7c4c3c458a338f17fc
Himanshu Joshi <himsjoshi27@gmail.com>
2020-04-30 17:04:55 +0530

    created lang files for dashboard and inquiry page

---
+++ resources/lang/en/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'dashboard' => "Dashboard",
+    'chart' => "Chart",
+    'table' => "Table",
+
+    // DataTable Heading
+    'name' => "Name",
+    'company' => "Company",
+    'state' => "State",
+    'city' => "City",
+    'email' => "Email",
+    'ph_no' => "Phone Number",
+];
---
+++ resources/lang/en/inquiry.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'inquiry_demo' => 'Product Demo Inquiry',
+    'full_name' => "Full Name",
+    'company_name' => "Company Name",
+    'state' => "State",
+    'city' => "City",
+    'email' => "Email",
+    'phone' => "Phone Number",
+    'submit' => "Submit",
+];
